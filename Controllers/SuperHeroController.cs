643880e1896d3ae8522b47618c92c7fd1f8d67d0
Musab-Al-omari <musabsomari@gmail.com>
2022-02-17 17:48:46 +0200

    CRUD API

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,77 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace superHeroApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heroes = new List<SuperHero>{
+                new SuperHero{Id=1,Name="MAN",FirstName="PETER",LastName="PARKER",Place="NEWYORK"},
+                new SuperHero{Id=2,Name="XX",FirstName="XX",LastName="XXXXC",Place="XXXX"},
+                new SuperHero{Id=3,Name="CC",FirstName="CCC",LastName="CCCCC",Place="CCCC"},
+            };
+        [HttpGet]
+        [Route("super-hero")]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heroes);
+        }
+
+        [HttpPost]
+        [Route("add-hero")]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+
+        [HttpPost]
+        [Route("test-hero")]
+        public async Task<ActionResult> TestHero([FromBody] SuperHero hero)
+        {
+            Console.WriteLine(hero.Id);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            return Ok();
+
+        }
+
+
+
+        [HttpGet]
+        [Route("single-hero/{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
+        {
+
+            var foundHero = heroes.Find(hero => hero.Id == id);
+            if (foundHero == null)
+            {
+                return BadRequest("hero dose not found");
+
+            }
+            return Ok(foundHero);
+        }
+
+        [HttpPut]
+        [Route("edit-hero/{id}")]
+        public async Task<ActionResult<SuperHero>> EditSingleHero(int id, [FromBody] SuperHero body)
+        {
+            var foundHero = heroes.Find(hero => hero.Id == id);
+            if (foundHero == null)
+            {
+                return BadRequest("hero dose not found");
+
+            }
+
+            foundHero.FirstName = body.FirstName;
+            foundHero.LastName = body.LastName;
+            foundHero.Name = body.Name;
+            foundHero.Place = body.Place;
+            return Ok(foundHero);
+        }
+        
+
+
+    }
+}
